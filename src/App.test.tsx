e0eaf8ce52cbd2cc8a01a09f8ef4292b4b6ad7fa
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-01-05 13:38:40 -0300

    fix: search integration test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
+import Providers from "./providers";
 import App from "./App";
 
 describe("Website main page", () => {
   test("Should be able to add a brazilian zipcode, that contains 8 characters", () => {
-    render(<App />);
+    render(
+      <Providers>
+        <App />
+      </Providers>
+    );
     const zipcodeField = screen.getByPlaceholderText("Insira o CEP");
     const searchButton = screen.getByText("Buscar pelo CEP");
 
